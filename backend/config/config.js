72cdda30e76fd60aa824a2509bcf6454a28890cc
Victor Mwangi <vickmwas@gmail.com>
2022-04-30 13:58:35 +0200

    Fixed Folder Problem

---
+++ backend/config/config.js
@@ -0,0 +1,7 @@
+// dbPassword = 'mongodb+srv://user:<password>@cluster0.1vxr7.mongodb.net/<database>?retryWrites=true&w=majority'
+dbPassword = 'mongodb+srv://vickmwas:nug21Rmek78y7ls8@cluster0.x0hgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+module.exports = {
+    'secret': '<password>',
+    mongoURI: dbPassword
+}
