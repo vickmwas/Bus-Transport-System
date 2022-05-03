af121c61f99b4ecfe2d5e7c2a803b1bcd513c24d
Victor K Mwangi <vickmwas@users.noreply.github.com>
2022-05-03 23:32:42 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,5 @@
 // dbPassword = 'mongodb+srv://user:<password>@cluster0.1vxr7.mongodb.net/<database>?retryWrites=true&w=majority'
-dbPassword = 'mongodb+srv://vickmwas:nug21Rmek78y7ls8@cluster0.x0hgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+dbPassword = '' //Enter Your Mongo DB Username
 
 module.exports = {
     'secret': '<password>',
